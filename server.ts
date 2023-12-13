c9cf0db28e74521101e91f2c9acf044fd56b2011
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2023-12-13 21:20:47 +0530

    Created files.ts with updating server.ts file

--- server.ts
+++ server.ts
@@ -2,9 +2,17 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import connectDb from "./config/db";
+import fileRoutes from "./routes/files";
+
 const app = express();
 dotenv.config();
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use("/api/files", fileRoutes)
 
 connectDb();
 const PORT = process.env.PORT || 4000;
