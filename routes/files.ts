c3da89b56a552341dd3d092f84f4a931c1691bd6
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2023-12-13 23:50:11 +0530

    update in server.js and files.js added the url of file

--- routes/files.ts
+++ routes/files.ts
@@ -1,16 +1,42 @@
 import express from "express"
 import multer from "multer";
 const router = express.Router();
+import {UploadApiResponse, v2 as cloudinary} from "cloudinary";
+import File from "../models/File";
 
 const storage = multer.diskStorage({})
     let upload = multer({
         storage,
     })
-router.post("/upload", upload.single("myFile"), (req, res) => {
+router.post("/upload", upload.single("myFile"), async(req, res) => {
     try{
         if(!req.file)
         return res.status(400).json({message: "Hey bro! We need the file"});
         console.log(req.file);
+
+        let uploadedFile: UploadApiResponse;
+        try{
+            uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+                folder:"FileShare",
+                resource_type:"auto",
+            })
+        }catch(err){
+            console.log(err);
+            return res.status(400).json({message: "Cloudinary Error"});
+        }
+        const {originalname} = req.file;
+        const {secure_url, bytes,format} = uploadedFile;
+
+        const file = await File.create({
+            filename:originalname,
+            sizeInBytes: bytes,
+            secure_url,
+            format,
+        })
+        res.status(200).json({
+            id:file._id,
+            downloadPageLink:`${process.env.API_BASE_ENDPOINT_CLIENT}download/${file._id}`,
+        });
     }catch(err){
         console.log(err);
         res.status(500).json({ message:"Server Error :(" });
--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import cors from "cors"
 import connectDb from "./config/db";
 import fileRoutes from "./routes/files";
-
+import {v2 as cloudinary} from "cloudinary";
 const app = express();
 dotenv.config();
 app.use(cors());
@@ -12,6 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_API_CLOUD,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 app.use("/api/files", fileRoutes)
 
 connectDb();
